6e70e681f6b919233d163881abe8b55d5417a57f
anagd <andelamo@myuax.es>
2023-09-27 17:48:13 +0200

    Prueba con main

--- src/main/java/Cuenta.java
+++ src/main/java/Cuenta.java
@@ -1,2 +1,19 @@
-public class Cuenta {
+class Cuenta {
+    private double saldo;
+
+    public Cuenta(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public synchronized void depositar(double cantidad) {
+        saldo += cantidad;
+    }
+
+    public synchronized void retirar(double cantidad) {
+        saldo -= cantidad;
+    }
+
+    public synchronized double obtenerSaldo() {
+        return saldo;
+    }
 }
--- src/main/java/Lanzador.java
+++ src/main/java/Lanzador.java
@@ -1,2 +1,39 @@
 public class Lanzador {
+    public static void main(String[] args) {
+        Cuenta cuenta = new Cuenta(10000);
+
+        Thread[] depositoThreads = new Thread[1200];
+        Thread[] retiroThreads = new Thread[1200];
+
+        for (int i = 0; i < 400; i++) {
+            depositoThreads[i] = new Thread(new HiloCliente(cuenta, 100, true));
+            depositoThreads[i + 400] = new Thread(new HiloCliente(cuenta, 50, true));
+            depositoThreads[i + 800] = new Thread(new HiloCliente(cuenta, 20, true));
+
+            retiroThreads[i] = new Thread(new HiloCliente(cuenta, 100, false));
+            retiroThreads[i + 400] = new Thread(new HiloCliente(cuenta, 50, false));
+            retiroThreads[i + 800] = new Thread(new HiloCliente(cuenta, 20, false));
+        }
+
+        for (int i = 0; i < 1200; i++) {
+            depositoThreads[i].start();
+            retiroThreads[i].start();
+        }
+
+        for (int i = 0; i < 1200; i++) {
+            try {
+                depositoThreads[i].join();
+                retiroThreads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        double saldoFinal = cuenta.obtenerSaldo();
+        if (saldoFinal == 10000) {
+            System.out.println("La simulación ha funcionado correctamente. Saldo final: " + saldoFinal);
+        } else {
+            System.out.println("La simulación ha fallado. Saldo final: " + saldoFinal);
+        }
+    }
 }
